6c827b69cd393f98c9e8edf43331a7fc4f7e74fe
Duane Knesek <duane.knesek@gmail.com>
2022-08-22 12:34:15 -0500

    Got split move to work with recursive mode

--- src/BlenderSplitter.js
+++ src/BlenderSplitter.js
@@ -1,6 +1,16 @@
-import { useState } from "react";
+import {useLayoutEffect, useRef, useState} from 'react';
 
 const BlenderSplitter = ( {children, layout} ) => {
+
+	const ref = useRef(null);
+	const [width, setWidth] = useState(0);
+	const [height, setHeight] = useState(0);
+
+	useLayoutEffect(() => {
+		setWidth(ref.current.offsetWidth);
+		setHeight(ref.current.offsetHeight);
+		console.log(`qwidth=${width} height=${height}`);
+	}, []);
 
 	const [layoutState, setLayoutState] = useState(layout);
 
@@ -30,8 +40,8 @@ const BlenderSplitter = ( {children, layout} ) => {
 
 	const reshapeMargin = 1;
 
-	const testNearSplit = percentVector => {
-		return testNearSplitRecursive(percentVector,layoutState.content,{x:reshapeMargin,y:reshapeMargin},layoutState.areTopLevelSplittersVertical);
+	const testNearSplit = (percentVector,forMove) => {
+		return testNearSplitRecursive(percentVector,layoutState.content,{x:reshapeMargin,y:reshapeMargin},layoutState.areTopLevelSplittersVertical,forMove);
 	}
 
 	const testWithinMargin = (one, two, margin) => Math.abs(one - two) <= margin;
@@ -60,7 +70,7 @@ const BlenderSplitter = ( {children, layout} ) => {
 		}
 
 		var foundComponentIndex;
-		if (foundSplitIndex==null) {
+		if (foundSplitIndex===null) {
 			foundComponentIndex = minIndex+1;
 		} else {
 			foundComponentIndex = null;
@@ -72,7 +82,42 @@ const BlenderSplitter = ( {children, layout} ) => {
 		}
 	}
 
-	const testNearSplitRecursive = (percentVector,content,margin,isCurrentSplitterVertical) => {
+	const convertVector = (minValue,maxValue,isVertical,vector) => {
+		const convFunction = val => 100*(val-minValue)/(maxValue-minValue);
+		var subVector;
+		if( isVertical ) {
+			subVector = {
+				x: convFunction(vector.x),
+				y: vector.y
+			};
+		} else {
+			subVector = {
+				x: vector.x,
+				y: convFunction(vector.y)
+			};
+		}
+		return subVector;
+	}
+
+	const convertMargin = (minValue,maxValue,isVertical,margin) => {
+		const marginFunction = val => val*100/(maxValue-minValue);
+		var subMargin;
+		if( isVertical ) {
+			subMargin = {
+				x: marginFunction(margin.x),
+				y: margin.y
+			};
+		} else {
+			subMargin = {
+				x: margin.x,
+				y: marginFunction(margin.y)
+			};
+		}
+		return subMargin;
+	}
+
+	const testNearSplitRecursive = (percentVector,content,margin,isCurrentSplitterVertical,returnFirst) => {
+		//console.log(`testNearSplitRecursive((${percentVector.x},${percentVector.y}),${arrayToString(content)},returnFirst=${returnFirst})`);
 		var valToTest;
 		var marginToTest;
 		if( isCurrentSplitterVertical ) {
@@ -103,7 +148,7 @@ const BlenderSplitter = ( {children, layout} ) => {
 			if( isArray ) {
 				const {foundSplitIndex,foundComponentIndex} = binarySearch(content,valToTest,marginToTest);
 				foundIndex = foundSplitIndex;
-				if( foundIndex==null ) {
+				if( foundIndex===null ) {
 					childCandidateIndices = [ foundComponentIndex ];
 				} else {
 					childCandidateIndices = [ foundSplitIndex-1, foundSplitIndex+1 ];
@@ -114,56 +159,40 @@ const BlenderSplitter = ( {children, layout} ) => {
 			}
 		}
 
+		//console.log("foundIndex="+foundIndex);
 		var returnMe = [];
-		if( isArray ) {
-			childCandidateIndices.forEach( childIndex => {
-				const minValue = childIndex===0 ? 0 : content[childIndex-1];
-				const maxValue = childIndex===length-1 ? length-1 : content[childIndex+1];
-
-				const convFunction = val => 100*(val-minValue)/(maxValue-minValue);
-				const marginFunction = val => val*100/(maxValue-minValue);
-
-				var subPercentVector;
-				var subMargin;
-				if( isCurrentSplitterVertical ) {
-					subPercentVector = {
-						x: convFunction(percentVector.x),
-						y: percentVector.y
-					};
-					subMargin = {
-						x: marginFunction(margin.x),
-						y: margin.y
-					};
-				} else {
-					subPercentVector = {
-						x: percentVector.x,
-						y: convFunction(percentVector.y)
-					};
-					subMargin = {
-						x: margin.x,
-						y: marginFunction(margin.y)
-					};
-				}
-
-				var recurseArray = testNearSplitRecursive(subPercentVector,content[childIndex],subMargin,!isCurrentSplitterVertical);
-				if( recurseArray.length>0 ) {
-					recurseArray.forEach( subArray => {
-						var myArray = [childIndex];
-						myArray = myArray.concat(subArray);
-						returnMe.push(myArray);
-					});
-				} else {
-					if( foundIndex!==null ) {
-						returnMe.push([foundIndex]);
-					}
-				}
-			});
+		if( returnFirst && foundIndex!=null ) {
+			returnMe.push([foundIndex]);
 		} else {
-			if( foundIndex!==null ) {
-				returnMe.push([foundIndex]);
+			if( isArray ) {
+				childCandidateIndices.forEach( childIndex => {
+					const minValue = childIndex===0 ? 0 : content[childIndex-1];
+					const maxValue = childIndex===length-1 ? length-1 : content[childIndex+1];
+
+					var subPercentVector = convertVector(minValue,maxValue,isCurrentSplitterVertical,percentVector);
+					var subMargin = convertMargin(minValue,maxValue,isCurrentSplitterVertical,margin);
+
+					var recurseArray = testNearSplitRecursive(subPercentVector,content[childIndex],subMargin,!isCurrentSplitterVertical,returnFirst);
+					if( recurseArray.length>0 ) {
+						recurseArray.forEach( subArray => {
+							var myArray = [childIndex];
+							myArray = myArray.concat(subArray);
+							returnMe.push(myArray);
+						});
+					} else {
+						if( foundIndex!==null ) {
+							returnMe.push([foundIndex]);
+						}
+					}
+				});
+			} else {
+				if( foundIndex!==null ) {
+					returnMe.push([foundIndex]);
+				}
 			}
 		}
 
+		//printArray("returnMe",returnMe);
 		return returnMe;
 	}
 
@@ -198,14 +227,78 @@ const BlenderSplitter = ( {children, layout} ) => {
 		console.log(`${label}=${arrayToString(a)}`);
 	}
 
-	const determineState = (e) => {
-		const percent = eventToPercentVector(e);
-		const nearSplit = testNearSplit(percent);
-		const length = nearSplit.length;
-		const stateEnum = state.stateEnum;
+	const pointToString = (p) => `(${p.x},${p.y})`;
 
-		// by default, assume old state
-		var nextState = state;
+	//const isMoveState = stateEnum =>
+	//	stateEnum===StateEnum.ElgibleToMoveforMove
+	//	|| stateEnum===StateEnum.AboutToMove
+	//	|| stateEnum===StateEnum.Moving
+
+	const isCopyState = stateEnum =>
+		stateEnum===StateEnum.ElgibleToCopy
+		|| stateEnum===StateEnum.AboutToCopy
+		|| stateEnum===StateEnum.Copying
+
+	const setToNoneState = () => {
+		setState( {
+			stateEnum: StateEnum.None
+		} );
+	}
+
+	const setToElgibleToMoveState = (split) => {
+		setState( {
+			stateEnum: StateEnum.ElgibleToMove,
+			split: split
+		} );
+	}
+
+	const setToAboutToMoveState = (split,pressSplitLocation,pressMouseLocation) => {
+		setState( {
+			stateEnum: StateEnum.AboutToMove,
+			split: split,
+			pressSplitLocation: pressSplitLocation,
+			pressMouseLocation: pressMouseLocation
+		} );
+	}
+
+	const setToMovingState = (split,pressSplitLocation,pressMouseLocation,currentMouseLocation) => {
+		setState( {
+			stateEnum: StateEnum.Moving,
+			split: split,
+			pressSplitLocation: pressSplitLocation,
+			pressMouseLocation: pressMouseLocation,
+			currentMouseLocation: currentMouseLocation
+		} );
+	}
+
+	const setToElgibleToCopyState = (split) => {
+		setState( {
+			stateEnum: StateEnum.ElgibleToCopy,
+			split: split
+		} );
+	}
+
+	const setToAboutToCopyState = (split,pressLocation) => {
+		setState( {
+			stateEnum: StateEnum.AboutToCopy,
+			split: split,
+			pressLocation: pressLocation
+		} );
+	}
+
+	const setToCopyingState = () => {
+		setState( {
+			stateEnum: StateEnum.Copying,
+			split: null,
+		} );
+	}
+
+	const determineState = (e) => {
+		const stateEnum = state.stateEnum;
+		const percent = eventToPercentVector(e);
+		const nearSplit = testNearSplit(percent,!isCopyState(stateEnum));
+		const length = nearSplit.length;
+		//printArray("nearSplit",nearSplit);
 
 		const isPressedNow = e.buttons &= 1;
 		if( isPressedNow ) {
@@ -215,76 +308,42 @@ const BlenderSplitter = ( {children, layout} ) => {
 					|| stateEnum===StateEnum.ElgibleToMove ) {
 				if(e.ctrlKey) {
 					if( length>0 && length<3 ) {
-						nextState = {
-							stateEnum: StateEnum.AboutToCopy,
-							nearSplit: nearSplit,
-							pressLocation: currentLocation
-						};
+						setToAboutToCopyState(nearSplit,currentLocation);
 					}
 				} else {
-					if( length>1 && length<3 ) {
-						nextState = {
-							stateEnum: StateEnum.AboutToMove,
-							nearSplit: nearSplit,
-							pressLocation: currentLocation
-						};
+					//if( length>1 && length<3 ) {
+					if( length>0 ) {
+						setToAboutToMoveState(nearSplit,getSplitLocation(nearSplit[0]),currentLocation);
 					}
 				}
 			} else if( stateEnum===StateEnum.AboutToCopy ) {
 				if( JSON.stringify(nearSplit) !== JSON.stringify(state.nearSplit) ) {
-					console.log("Copying!!");
-					nextState = {
-						stateEnum: StateEnum.Copying,
-						split: null,
-					}
+					setToCopyingState();
 				}
 
 			} else if( stateEnum===StateEnum.AboutToMove ) {
+				console.log(`nearSplit=${nearSplit} state.nearSplit=${state.nearSplit}`);
 				if( JSON.stringify(nearSplit) !== JSON.stringify(state.nearSplit) ) {
-					var minSplit= state.nearSplit[0].length < state.nearSplit[1].length ? state.nearSplit[0] : state.nearSplit[1];
-					printArray("nearSplit",state.nearSplit);
-					printArray("minSplit",minSplit);
-					nextState = {
-						stateEnum: StateEnum.Moving,
-						split: minSplit,
-						pressLocation: state.pressLocation,
-						currentLocation: percent
-					}
-					console.log("Moving split="+nextState.split+"!!");
+					//var minSplit= state.nearSplit[0].length < state.nearSplit[1].length ? state.nearSplit[0] : state.nearSplit[1];
+					//printArray("minSplit",minSplit);
+					setToMovingState(state.split[0],state.pressSplitLocation,state.pressMouseLocation,percent);
+					//console.log("Moving split="+nextState.split+"!!");
 				}
 			} else if( stateEnum===StateEnum.Copying ) {
 			} else if( stateEnum===StateEnum.Moving ) {
-				nextState = {
-					stateEnum: StateEnum.Moving,
-					split: state.split,
-					pressLocation: state.pressLocation,
-					currentLocation: percent
-				}
+				setToMovingState(state.split,state.pressSplitLocation,state.pressMouseLocation,percent);
 			}
 		} else if(e.ctrlKey) {
 			if( length>0 && length<3 ) {
-				nextState = {
-					stateEnum: StateEnum.ElgibleToCopy,
-					nearSplit: nearSplit
-				};
+				setToElgibleToCopyState(nearSplit);
 			} else {
-				nextState = {
-					stateEnum: StateEnum.None
-				};
+				setToNoneState();
 			}
-		} else if( length!==2 ) {
-			nextState = {
-				stateEnum: StateEnum.None
-			};
+		} else if( length===0 ) {
+			setToNoneState();
 		} else {
-			nextState = {
-				stateEnum: StateEnum.ElgibleToMove,
-				nearSplit: nearSplit
-			};
+			setToElgibleToMoveState(nearSplit);
 		}
-
-		console.log("state="+nextState.stateEnum);
-		setState( nextState );
 	}
 
 	const updateCursor = e => {
@@ -299,7 +358,7 @@ const BlenderSplitter = ( {children, layout} ) => {
 				isVertical = false;
 			}
 
-			if( state.nearSplit[0].length%2 === 0 ) {
+			if( state.split[0].length%2 === 0 ) {
 				isVertical = !isVertical;
 			}
 
@@ -326,27 +385,101 @@ const BlenderSplitter = ( {children, layout} ) => {
 		}
 	}
 
-	const moveSplit = (split, pressLocation, currentLocation) => {
+	const getSplitLocation = split => {
+		var current = layoutState.content;
+		split.forEach( element => {
+			current = current[element];
+		} );
+		return current;
+	}
+
+	const setSplitLocation = (split,loc) => {
+		var copy = deepContentCopy(layoutState.content);
+		var copyLayoutState = {
+			areTopLevelSplittersVertical: layoutState.areTopLevelSplittersVertical,
+			content: copy
+		};
+
+		var contentElement = copy;
+		var parent = copy;
+		for( var i=0;i<split.length-1;i++) {
+			parent = parent[ split[i] ];
+		}
+		const last = split[ split.length-1 ];
+		parent[ last ] = loc
+		setLayoutState(copyLayoutState);
+	}
+
+	const moveSplit = (movingState) => {
 		var isVertical = layoutState.areTopLevelSplittersVertical;
-		if( split.length%2==0 )
-			isVertical = !isVertical;
+		//if( split.length%2===0 )
+		//	isVertical = !isVertical;
 
 		var copy = deepContentCopy(layoutState.content);
-		var contentElement = copy;
-		split.forEach( e => contentElement = contentElement[e] );
-		contentElement+=1;
-		console.log(`contentElement=${contentElement}`);
-		setLayoutState(deepContentCopy);
+		var copyLayoutState = {
+			areTopLevelSplittersVertical: layoutState.areTopLevelSplittersVertical,
+			content: copy
+		};
+
+		const pressSplitLocation = movingState.pressSplitLocation;
+		const pressMouseLocation = movingState.pressMouseLocation;
+		var currentMouseLocation = movingState.currentMouseLocation;
+
+		//console.log(`pressMouseLocation=${JSON.stringify(pressMouseLocation)} currentMouseLocation=${JSON.stringify(currentMouseLocation)}`);
+		//var percentOffsetVector = {
+		//	x: currentMouseLocation.x-pressMouseLocation.x,
+		//	y: currentMouseLocation.y-pressMouseLocation.y
+		//};
+
+		//console.log(`percentOffsetVector=${pointToString(percentOffsetVector)}`);
+
+		//var splitLength = currentState.currentMouseInfo.maxSplit - currentState.currentMouseInfo.minSplit;
+		//var percentOffset = pixelOffset*100/splitLength;
+		//var newPercentages = [...currentState.splitPercentagesOnPress]
+
+		const split = movingState.split;
+		var parent = layoutState.content;
+		//printArray("split",split);
+		for( var i=0;i<split.length-1;i++) {
+			const component = split[i];
+			var minValue;
+			var maxValue;
+			if(parent.length==1) {
+				minValue = 0;
+				maxValue = 100;
+			} else {
+				minValue = parseFloat( parent[ component - 1 ] );
+				maxValue = parseFloat( parent[ component + 1 ] );
+			}
+			//console.log(`minValue=${minValue} maxValue=${maxValue}`);
+			currentMouseLocation = convertVector(minValue,maxValue,isVertical,currentMouseLocation);
+			//console.log(`currentMouseLocation=${pointToString(currentMouseLocation)}`);
+			isVertical = !isVertical;
+			parent = parent[ component ];
+		}
+		const last = split[ split.length-1 ];
+
+//		const componentOffset = isVertical ? percentOffsetVector.x : percentOffsetVector.y;
+//		parent[ last ] = parent[ last ] + componentOffset;
+//		//console.log(`contentElement=${contentElement}`);
+//		printArray("aboutToSet",copyLayoutState.content);
+//		setLayoutState(copyLayoutState);
+		//var loc=movingState.pressSplitLocation;
+		//console.log("loc before="+loc);
+		var loc = isVertical ? currentMouseLocation.x : currentMouseLocation.y;
+		//console.log("loc="+loc);
+		setSplitLocation(movingState.split,loc);
 	}
 
 	const mouseMoved = (e) => {
 		determineState(e);
 		updateCursor(e);
 
-		//printArray("mouseMovedSplit",state.split);
-
-		if( state.stateEnum==StateEnum.Moving ) {
-			moveSplit(state.split,state.pressLocation,state.currentLocation);
+		if( state.stateEnum===StateEnum.Moving ) {
+			//printArray("before move",layoutState.content);
+			//printArray("split",state.split);
+			moveSplit(state);
+			//printArray("after move",layoutState.content);
 		}
 	}
 
@@ -365,8 +498,9 @@ const BlenderSplitter = ( {children, layout} ) => {
 	}
 
 	const generateDOM = layoutState => {
+		//printArray("aboutToGenerateDom",layoutState.content);
 		return (
-		<div style={{ width: "100%", height: "100%" }}
+		<div ref={ref} style={{ width: "100%", height: "100%" }}
 				tabIndex={0}
 				onKeyDown={keyDown}
 				onMouseMove={mouseMoved}
@@ -392,6 +526,7 @@ const BlenderSplitter = ( {children, layout} ) => {
 				var elements=[{
 					child: generateContent(layoutState[0],!isCurrentSplitterVertical)
 				}];
+				//printArray("aboutToGenerate",layoutState);
 				for(var i=1;i<layoutState.length;i++) {
 					var element = layoutState[i];
 					const elementPercent = element-lastPercent;
@@ -399,7 +534,7 @@ const BlenderSplitter = ( {children, layout} ) => {
 					lastPercent += elementPercent;
 					i++;
 					if(i>=layoutState.length) {
-						throw "Expect content array to have odd number of elements.  Children name strings with splitter percentages between.";
+						throw "Expect content array to have odd number of elements.  Children name strings with splitter percentages between i="+i+" length="+layoutState.length+".";
 					}
 					elements.push( {
 						child: generateContent(layoutState[i],!isCurrentSplitterVertical)
